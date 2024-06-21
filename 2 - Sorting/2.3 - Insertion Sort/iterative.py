074c01b0a0a1889db42fe9fc9f083e27f1c020f3
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2024-06-21 08:44:35 +0000

    Implement insertion sort

--- 2 - Sorting/2.3 - Insertion Sort/iterative.py
+++ 2 - Sorting/2.3 - Insertion Sort/iterative.py
@@ -1,20 +1,24 @@
 # Write a function that takes an array of integers and sorts it using the insertion sort algorithm.
 
 # Example
-# insertion_sort([64, 25, 12, 22, 11], 5) -> [11, 12, 22, 25, 64]
+# insertion_sort([64, 25, 12, 22, 11]) -> [11, 12, 22, 25, 64]
 
 # Time complexity: O(n^2)
 # Space complexity: O(1)
 
 # How does it work?
-# We iterate over the array starting from the second element.
+# Insertion Sort works by iterating over the array starting from the second element.
 # We store the current element in a variable key.
 # We iterate over the array from the current element to the first element.
 # If the current element is less than the key, we shift the elements to the right.
 # We insert the key at the correct position.
 # We repeat the process until the array is sorted.
 
-def insertion_sort(arr: list[int], n: int) -> None:
+# Note: This is a stable sorting algorithm, meaning that the relative order of equal elements is preserved.
+
+def insertion_sort(arr: list[int]) -> None:
+    n = len(arr)
+    
     for i in range(1, n):
         key = arr[i]
         j = i - 1
@@ -24,3 +28,4 @@ def insertion_sort(arr: list[int], n: int) -> None:
             j -= 1
             
         arr[j + 1] = key
+        
