fe50a72431060a32e8bfc4c18585e3bcf061d67f
Ajaz Ur Rehman <ajazurrehman4@gmail.com>
2024-06-26 10:22:51 +0000

    Add largest element

---
+++ 3 - Arrays/3.1 - Easy/3.1.1 - Largest Element in Array/solution.py
@@ -0,0 +1,23 @@
+# Write a function that returns the largest element in a list of integers.
+
+# Example
+# largest_element([64, 25, 12, 22, 11], 5) -> 64
+
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+# How does it work?
+# We iterate over the array and store the largest element in a variable.
+# We return the largest element.
+
+def largest_element(arr: list[int], n: int) -> int:
+    if n == 0:
+        return -1
+    
+    res = arr[0]
+    
+    for i in range(1, n):
+        if arr[i] > res:
+            res = arr[i]
+            
+    return res
